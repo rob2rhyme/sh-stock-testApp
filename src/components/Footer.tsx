b9517fe163285b9d71e6f5c4b370ae2d798349fe
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-04-11 00:01:08 -0400

    corrected data typos and updated github data link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
       <div className={styles.footerContent}>
         <span>© {currentYear} | Made for Smokers Haven by </span>
         <a 
-          href="https://github.com/robin-thapa" 
+          href="https://github.com/rob2rhyme/sh-stock-tracking/tree/main/src/data" 
           target="_blank" 
           rel="noopener noreferrer"
           className={styles.link}
