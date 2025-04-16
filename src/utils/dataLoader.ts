8b53b0baece7d535b5a71a8f00727009c26442db
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-04-16 15:14:36 -0400

    Update dataLoader.ts

--- src/utils/dataLoader.ts
+++ src/utils/dataLoader.ts
@@ -1,14 +1,15 @@
 import { ProductCategory } from '../components/types';
+
 // Import your JSON data files
 import gb15k from '../data/gb-15k.json';
 import gb25k from '../data/gb-25k.json';
 import cali8k from '../data/cali-8k.json';
 import cali20k from '../data/cali-20k.json';
-import yovo18k from '../data/Yovo-18k.json';
+import yovo18k from '../data/yovo-18k.json';
 import yovo8k from '../data/Yovo-8k.json';
 import fogerFull from '../data/Foger-full.json';
 import fogerPodOnly from '../data/FogerPodOnly.json';
-import raz30k from '../data/Raz30k.json';
+import Raz25K from '../data/Raz-25K.json';
 
 export const fetchAllData = async (): Promise<Record<string, ProductCategory>> => {
   const data: Record<string, ProductCategory> = {};
@@ -29,11 +30,11 @@ export const fetchAllData = async (): Promise<Record<string, ProductCategory>> =
   data['GeekBar 25K'] = processFile(gb25k, 'geekbar25k');
   data['Yovo 8K'] = processFile(yovo8k, 'Yovo-8k');
   data['Yovo Ultra 18K'] = processFile(yovo18k, 'Yovo-18k');
+  data['Raz 25K'] = processFile(Raz25K, 'Raz-25K');
   // data['Cali 8K'] = processFile(cali8k, 'cali8k');
   // data['Cali 20K'] = processFile(cali20k, 'cali20k');
   // data['Foger Full'] = processFile(fogerFull, 'FogerFull');
   // data['Foger Pod Only'] = processFile(fogerPodOnly, 'FogerPodOnly');
-  // data['Raz 30K'] = processFile(raz30k, 'Raz30K');
 
   return data;
 };
