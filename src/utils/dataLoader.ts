0cad5c5bf615aa2eacc2a97076e4ce90e4bc3bee
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-04-17 17:52:48 -0400

    Update dataLoader.ts

--- src/utils/dataLoader.ts
+++ src/utils/dataLoader.ts
@@ -28,15 +28,15 @@ export const fetchAllData = async (): Promise<Record<string, ProductCategory>> =
     };
   };
 
-  data['GeekBar 15K'] = processFile(gb15k, 'geekbar15k');
-  data['GeekBar 25K'] = processFile(gb25k, 'geekbar25k');
-  data['Yovo 8K'] = processFile(yovo8k, 'Yovo-8k');
-  data['Yovo Ultra 18K'] = processFile(yovo18k, 'Yovo-18k');
-  data['Lost Mary 20K'] = processFile(lostMary20k, 'lostMary20k');
-  data['Raz 25k'] = processFile(raz25k, 'raz25k');
   data['Cali 8K'] = processFile(cali8k, 'cali8k');
-  data['Cali 20K'] = processFile(cali20k, 'cali20k');
-  data['RAMA 16K'] = processFile(rama16k, 'rama16k');
+data['Cali 20K'] = processFile(cali20k, 'cali20k');
+data['GeekBar 15K'] = processFile(gb15k, 'geekbar15k');
+data['GeekBar 25K'] = processFile(gb25k, 'geekbar25k');
+data['Lost Mary 20K'] = processFile(lostMary20k, 'lostMary20k');
+data['RAMA 16K'] = processFile(rama16k, 'rama16k');
+data['Raz 25k'] = processFile(raz25k, 'raz25k');
+data['Yovo 8K'] = processFile(yovo8k, 'Yovo-8k');
+data['Yovo Ultra 18K'] = processFile(yovo18k, 'Yovo-18k');
   // data['Foger Full'] = processFile(fogerFull, 'FogerFull');
   // data['Foger Pod Only'] = processFile(fogerPodOnly, 'FogerPodOnly');
 
