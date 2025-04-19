8b188588457293e6322b55e275fadebca0754367
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-04-19 19:03:25 -0400

    Update dataLoader.ts

--- src/utils/dataLoader.ts
+++ src/utils/dataLoader.ts
@@ -42,8 +42,6 @@ data['Yovo 8K'] = processFile(yovo8k, 'Yovo-8k');
 data['Yovo Ultra 18K'] = processFile(yovo18k, 'Yovo-18k');
 data['Pillow Talk 40K'] = processFile(pillowTalk40k, 'pillowTalk40k');
 data['Foger With Kit 30K'] = processFile(fogerWithKit30k, 'fogerWithKit30k');
-  // data['Foger Full'] = processFile(fogerFull, 'FogerFull');
-  // data['Foger Pod Only'] = processFile(fogerPodOnly, 'FogerPodOnly');
 
   return data;
 };
