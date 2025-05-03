8e1c504f23fc1d9f913ed1f3b6a2cf89589c18be
Robin G Thapa <61993378+rob2rhyme@users.noreply.github.com>
2025-05-03 12:44:30 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,12 +20,9 @@ export default function App({ Component, pageProps }: AppProps) {
       icon: "❌",
     },
   }}/>
-      <>
-        <Head>
-        <link rel="icon" href="/logo.png" />
-      </Head>
+     
       <Component {...pageProps} />
-      </>
+   
     </AuthProvider>
   );
 }
